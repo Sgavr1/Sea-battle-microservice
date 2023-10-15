d6cb5eb32ab1ba8c10f31348ca0d4af3f0b6dfa3
Sgavr1 <sanya.gavrilko60@gmail.com>
2023-10-15 03:44:26 +0300

    Begin

---
+++ src/test/java/com/sea/battle/microservice/SeaBattle/SeaBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sea.battle.microservice.SeaBattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
